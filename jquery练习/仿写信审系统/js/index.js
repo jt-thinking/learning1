08d884c5becc21003b8c3e6064f03772a0e2f862
jiangtao <jiangtao@anfast.com>
2017-10-20 16:42:56 +0800

    a

---
+++ jquery练习/仿写信审系统/js/index.js
@@ -0,0 +1,18 @@
+$(".login-button").click(function() {
+  $.ajaxSetup({
+    async:false
+  });
+  $.ajax({
+
+    crossDomain: true,
+    url: "http://192.168.1.203:9180/login/signIn?jobNumber=9200&password=202CB962AC59075B964B07152D234B70",
+    dataType: "json",
+    method: "POST",
+    // contentType:"application/json",
+
+    success: function(data) {
+      alert();
+    }
+  })
+  // $(location).attr("href", "index-dashboad.html");
+});
