4e4a4bc0868f8e1e95ed5095869178b9bf68d342
jiangtao <jiangtao@anfast.com>
2017-11-07 14:47:36 +0800

    11.7

---
+++ nginx-1.13.6/html/js/app.js
@@ -0,0 +1,53 @@
+    var app = new Vue({
+        el: '#app',
+        data: {
+            message:
+             '<img class="logo"src="img/logo2.jpg"><!--头部导航--><nav class="nav nav-pills justify-content-center"><a class="nav-link  "href="">首页</a><a class="nav-link nav-item"href="#">数据统计</a><a class="nav-link nav-item"href="#">用户系统</a><a class="nav-link nav-item "href="#">客户管理</a><a class="nav-link nav-item active"href="#">营销管理</a><a class="nav-link nav-item "href="#">业务管理</a><a class="nav-link nav-item "href="#">特殊业务</a></nav><hr><!--左侧导航--><ul class="nav  nav-pills flex-column left-nav"><br><br><li class="nav-item"><a class="nav-link active"href="#">客户查询</a></li><br/><li class="nav-item"><a class="nav-link"href="#">交易记录查询</a></li><br/><li class="nav-item"><a class="nav-link"href="#">投资查询</a></li><br/><li class="nav-item"><a class="nav-link "href="#">借款查询</a></li><br/><li class="nav-item"><a class="nav-link"href="">还款查询</a></li><br/><li class="nav-item"><a class="nav-link"href="">回款查询</a></li><br/><li class="nav-item"><a class="nav-link"href="">授信查询</a></li><br/><li class="nav-item"><a class="nav-link"href="">余额查询</a></li><br/><li class="nav-item"><a class="nav-link"href="">对账单</a></li><br/></ul>'
+        }
+    })
+
+
+    //     <img class="logo" src="img/logo2.jpg">
+    // <!-- 头部导航 -->
+    // <nav class="nav nav-pills justify-content-center">
+    //     <a class="nav-link  " href="">首页</a>
+    //     <a class="nav-link nav-item" href="#">数据统计</a>
+    //     <a class="nav-link nav-item" href="#">用户系统</a>
+    //     <a class="nav-link nav-item " href="#">客户管理</a>
+    //     <a class="nav-link nav-item active" href="#">营销管理</a>
+    //     <a class="nav-link nav-item " href="#">业务管理</a>
+    //     <a class="nav-link nav-item " href="#">特殊业务</a>
+    // </nav>
+    // <hr /">
+    // <!-- 左侧导航 -->
+    // <ul class="nav  nav-pills flex-column left-nav">
+    //     <br>
+    //     <br>
+    //     <li class="nav-item">
+    //         <a class="nav-link active" href="#">客户查询</a>
+    //     </li>
+    //     <br/>
+    //     <li class="nav-item">
+    //         <a class="nav-link" href="#">交易记录查询</a>
+    //     </li>
+    //     <br/>
+    //     <li class="nav-item">
+    //         <a class="nav-link" href="#">投资查询</a>
+    //     </li>
+    //     <br/>
+    //     <li class="nav-item">
+    //         <a class="nav-link " href="#">借款查询</a>
+    //     </li>
+    //     <br/>
+    //     <li class="nav-item"><a class="nav-link" href="">还款查询</a></li>
+    //     <br/>
+    //     <li class="nav-item"><a class="nav-link" href="">回款查询</a></li>
+    //     <br/>
+    //     <li class="nav-item"><a class="nav-link" href="">授信查询</a></li>
+    //     <br/>
+    //     <li class="nav-item"><a class="nav-link" href="">余额查询</a></li>
+    //     <br/>
+    //     <li class="nav-item"><a class="nav-link" href="">对账单</a></li>
+    //     <br/>
+    // </ul>
+
