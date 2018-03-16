fc3a8bc27362186418f40b4f289a8da356627062
jiangtao <jiangtao@anfast.com>
2018-03-16 13:48:27 +0800

    no message

---
+++ vue ui 组件库/信审系统3.0 vue文件模板.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <!-- 表单区 -->
+
+    <!-- 表格区 -->
+  </div>
+</template>
+<script>
+export default {
+  components: {
+    // 'user-info': UserInfo,
+    // 'credit-info':CreditInfo
+
+  },
+  data () {
+    return {
+      // 外部组件区
+      message: undefined,
+      NotificationType: undefined
+
+      // tab区
+
+      // 表单区
+
+      // 表格区
+
+      // 分页区
+    }
+  },
+  created: function () {
+
+  },
+  mounted: function () {
+
+  },
+  computed: {
+  },
+  methods: {
+
+    // 外部组件区
+
+    // 通知组件
+    Notification () {
+      const h = this.$createElement
+
+      this.$notify({
+        title: '',
+        message: h('i', { style: 'color: teal' }, this.message),
+        type: this.NotificationType
+        //  success/warning/info/error
+      })
+    }
+
+    /*this.$http.get(url, { params }).then(res => {
+      if (res.body.code == "200") {
+
+      }
+
+    })*/
+
+    // 表单区
+
+    // 表格区
+
+    // 分页区
+
+  }
+}
+</script>
+<style scoped>
+
+</style>
+<style scoped>
+
+</style>
